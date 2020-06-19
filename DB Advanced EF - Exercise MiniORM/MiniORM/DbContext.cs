84571e6e1d6139fb4dfce996d269e0dc281b8cbd
mindball <mincho.balaliew@gmail.com>
2020-06-19 13:29:44 +0300

    .

--- DB Advanced EF - Exercise MiniORM/MiniORM/DbContext.cs
+++ DB Advanced EF - Exercise MiniORM/MiniORM/DbContext.cs
@@ -1,4 +1,88 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MiniORM
 {
-	// TODO: Create your DbContext class here.
+    public abstract class DbContex
+    {
+        private readonly DatabaseConnection connection;
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+        internal static Type[] AllowedSqlTypes =
+            {
+                typeof(bool),
+                typeof(int),
+                typeof(string),
+                typeof(uint),
+                typeof(DateTime),
+                typeof(double),
+                typeof(decimal),
+                typeof(long),
+                typeof(ulong)
+            };
+
+        protected DbContex(string connectionString)
+        {
+            this.connection = new DatabaseConnection(connectionString);
+            this.dbSetProperties = this.DiscoverDbSets();
+
+            using (new ConnectionManager(connection))
+            {                
+                this.InitializeDbSets();
+            }
+
+            this.MapAllRelations();
+        }
+
+        private void MapAllRelations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            var dbSets = this.dbSetProperties
+                .Select(x => x.Value.GetValue(this))
+                .ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSets)
+            {
+                var invalidEntities = dbSet
+                    .Where(x => !IsObjectValid(x))
+                    .ToArray();
+
+                if(invalidEntities.Any())
+                {
+                    throw new InvalidOperationException(
+                        $"{invalidEntities.Length} Invalid Entities found in {dbSet.GetType().Name}!");
+                }
+            }
+
+            using (new ConnectionManager(this.connection))
+            {
+                using (var transaction = this.connection.StartTransaction())
+                {
+                    foreach (var dbSet in dbSets)
+                    {
+                        var dbSetType = dbSet.GetType().GetGenericArguments().First();
+
+                        var persistMethod = typeof(DbContex)
+                            .GetMethod("Persist", BindingFlags.NonPublic | BindingFlags.Instance);
+                    }
+                }
+            }
+        }
+
+        private void InitializeDbSets()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<Type, PropertyInfo> DiscoverDbSets()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
--- DB Advanced EF - Exercise MiniORM/MiniORM/DbSet.cs
+++ DB Advanced EF - Exercise MiniORM/MiniORM/DbSet.cs
@@ -1,4 +1,85 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
 {
-	// TODO: Create your DbSet class here.
+	public class DbSet<TEntity> : ICollection<TEntity>
+        where TEntity : class, new()
+    {
+        internal DbSet(IEnumerable<TEntity> entities)
+        {
+            this.Entities = new List<TEntity>();
+            this.ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+
+        public int Count
+            => this.Entities.Count();
+        public bool IsReadOnly
+            => this.Entities.IsReadOnly;
+
+        internal ChangeTracker<TEntity> ChangeTracker { get; set; }
+
+        internal IList<TEntity> Entities { get; set; }
+
+        public void Add(TEntity item)
+        {
+            if(item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null");
+            }
+
+            this.Entities.Add(item);
+
+            this.ChangeTracker.Add(item);
+        }
+
+        public void Clear()
+        {
+            while (this.Entities.Any())
+            {
+                var current = this.Entities.First();
+                this.Remove(current);
+            }
+        }
+
+        public bool Contains(TEntity item)
+            => this.Entities.Contains(item);
+
+        public void CopyTo(TEntity[] array, int arrayIndex)
+            => this.Entities.CopyTo(array, arrayIndex);
+
+        public bool Remove(TEntity item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null");
+            }
+
+            bool isRemoved = this.Entities.Remove(item);
+            if(isRemoved)
+            {
+                this.ChangeTracker.Remove(item);
+            }
+
+            return isRemoved;
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return this.Entities.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => this.GetEnumerator();      
+        
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                this.Remove(entity);
+            }
+        }
+    }
 }
