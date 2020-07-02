8f8b85a094e5c261d61b51db95bc4c9185927618
mindball <mincho.balaliew@gmail.com>
2020-07-02 16:07:07 +0300

    exercises

--- Introduction to EF/Exercise/Exercises/Exercises/Data/SoftUni2EFContext.cs
+++ Introduction to EF/Exercise/Exercises/Exercises/Data/SoftUni2EFContext.cs
@@ -25,7 +25,11 @@ namespace Exercises.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=;Database=SoftUni2EF;User=sa;Password=");
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Introduction to EF/Exercise/Exercises/Exercises/StartUp.cs
+++ Introduction to EF/Exercise/Exercises/Exercises/StartUp.cs
@@ -18,20 +18,23 @@ namespace Exercises
                   new DbContextOptionsBuilder<SoftUni2EFContext>();
             options.UseLoggerFactory(MyLoggerFactory());
 
-            //Task3
-            //using (var context = new SoftUni2EFContext(options.Options))
-            //{
-            //    var result = GetEmployeesFullInformation(context);
-            //    Console.WriteLine(result);
-            //}
-
-            //Task4
             using (var context = new SoftUni2EFContext(options.Options))
             {
-                var result = GetEmployeesWithSalaryOver50000(context);
+                //Task3
+                //    var result = GetEmployeesFullInformation(context);               
+
+                //Task4
+                //var result = GetEmployeesWithSalaryOver50000(context);                
+
+                //Task5
+                //var result = GetEmployeesFromResearchAndDevelopment(context);
+
+                //Task6
+                var result = AddNewAddressToEmployee(context);
                 Console.WriteLine(result);
             }
         }
+
 
         private static ILoggerFactory MyLoggerFactory()
         {
@@ -54,6 +57,16 @@ namespace Exercises
 
             var empls = context
                 .Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.MiddleName,
+                    x.Department,
+                    x.JobTitle,
+                    x.Salary,
+                    x.EmployeeId
+                })
                 .OrderBy(x => x.EmployeeId)
                 .ToList();
 
@@ -70,12 +83,16 @@ namespace Exercises
         public static string GetEmployeesWithSalaryOver50000(SoftUni2EFContext context)
         {
             StringBuilder sb = new StringBuilder();
-
             var empls = context
                 .Employees
                 .Where(x => x.Salary > 50000)
+                .Select(x => new
+                {
+                    x.FirstName,                   
+                    x.Salary,                    
+                })                
                 .OrderBy(x => x.FirstName)
-                .ToList();
+                .ToList();           
 
             foreach (var employee in empls)
             {
@@ -84,6 +101,69 @@ namespace Exercises
 
             return sb.ToString().TrimEnd();
 
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUni2EFContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var empls = context.Employees
+                .Where(x => x.Department.Name == "Research and Development")
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.Salary,
+                    DepartmentName = x.Department.Name
+                }
+                )
+                .OrderBy(s => s.Salary)
+                .ThenBy(s => s.FirstName);
+
+            foreach (var employee in empls)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} " +
+                    $" from {employee.DepartmentName} - '$'{employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        private static string AddNewAddressToEmployee(SoftUni2EFContext softUni2EFContext)
+        {
+
+
+            var newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            //EF know this is new address, 
+            //softUni2EFContext.Add(newAddress);
+
+            var nakov = softUni2EFContext
+                .Employees
+                .FirstOrDefault(x => x.LastName == "Nakov");
+
+            //EF know this is new address
+            nakov.Address = newAddress;
+            softUni2EFContext.SaveChanges();
+
+            var employess = softUni2EFContext
+                .Employees
+                .OrderByDescending(x => x.AddressId)
+                .Select(a => a.Address.AddressText)
+                .Take(10)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employess)
+            {
+                sb.AppendLine($"{employee}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
