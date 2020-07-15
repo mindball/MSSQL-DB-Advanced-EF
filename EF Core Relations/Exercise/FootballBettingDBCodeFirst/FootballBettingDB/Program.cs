4f84f2b3a5c7ce47e7a0cc7351b94961425e7c19
mindball <mincho.balaliew@gmail.com>
2020-07-15 07:45:28 +0300

    Starting exercise Table Relations and completed project to replace the names of directories from a submitted list. The contents of each renamed directory are changed with the corresponding directory name, if there are more suffixes are added

---
+++ EF Core Relations/Exercise/FootballBettingDBCodeFirst/FootballBettingDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FootballBettingDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
