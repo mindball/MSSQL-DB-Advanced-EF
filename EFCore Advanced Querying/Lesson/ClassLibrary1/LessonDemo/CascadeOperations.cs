cf4348ecaca21cf30d10ebdd94d92d34de13430f
mindball <mincho.balaliew@gmail.com>
2020-07-31 09:01:09 +0300

    finish lesson

---
+++ EFCore Advanced Querying/Lesson/ClassLibrary1/LessonDemo/CascadeOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessonDemo
+{
+    class CascadeOperations
+    {
+        /*
+         * Required FK with cascade delete set to true, deletes
+            everything related to the deleted property
+
+         * Required FK with cascade delete set to false, throws exception
+            (it cannot leave the navigational property with no value)
+
+         * Optional FK with cascade delete set to true, deletes everything
+                related to the deleted property.
+
+         * Optional FK with cascade delete set to false, sets the value of
+            the FK to NULL
+        */
+
+        // Using OnDelete with DeleteBehavior Enumeration:
+        //DeleteBehavior.Cascade
+        //Deletes related entities(default for required FK)
+
+        //DeleteBehavior.Restrict
+        //Throws exception on delete
+
+        //DeleteBehavior.SetNull
+        //Sets the property to null (affects database)
+
+            /* Some Code:
+             * 
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Replies)
+                .WithOne(a => a.Author)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Replies)
+                .WithOne(a => a.Author)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            */
+
+    }
+}
---
+++ EFCore Advanced Querying/Lesson/Readme.md
@@ -0,0 +1,14 @@
+# Advanced Entity Framework Core
+### Executing Native SQL Queries
+
+## Execute Stored Procedures
+
+## Object State Tracking
+
+## Bulk Operations
+
+## Types of Loading
+
+## Concurrency Checks
+
+## Cascade Operations
