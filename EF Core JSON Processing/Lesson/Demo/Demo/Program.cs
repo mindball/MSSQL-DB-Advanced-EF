b8059900b4f1efd14622091ab1c7258c2be7ed03
mindball <mincho.balaliew@gmail.com>
2020-08-14 09:53:57 +0300

    finish lessons

---
+++ EF Core JSON Processing/Lesson/Demo/Demo/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weather = new WeatherForeCast();
+
+            DeSerializeFileMicrosoftTextJson("weather");
+            SerializeFileMicrosoftTextJson(weather);
+
+            DeSerializeFileNewtonsoftJson("weather");
+            SerializeFileNewtonsoftJson(weather);
+
+            //Configuring JSON.NET
+            FormatJSON(weather);
+            DeserializeAnonymousType("weather");
+            ParsingОfObjects(weather);
+
+        }
+
+        private static void ParsingОfObjects(WeatherForeCast weather)
+        {
+            //List of settings
+            var jsonSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy()
+                },
+                Formatting = Formatting.Indented,
+                Culture = CultureInfo.InvariantCulture
+                //and so on....
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(weather, jsonSettings));
+        }
+
+        private static void DeserializeAnonymousType(string fileName)
+        {
+            var partOfData = new { TempetureC = 0, Summary = string.Empty };
+
+            var file = File.ReadAllText(fileName);
+
+            partOfData = JsonConvert.DeserializeAnonymousType(file, partOfData);
+
+            Console.WriteLine($"{partOfData.TempetureC} {partOfData.Summary}");
+
+        }
+
+        private static void FormatJSON(WeatherForeCast weather)
+        {
+            //Human readable
+            Console.WriteLine(JsonConvert.SerializeObject(weather, Formatting.Indented));
+        }
+
+        private static void SerializeFileNewtonsoftJson(WeatherForeCast weather)
+        {
+            var convertToJson = JsonConvert.SerializeObject(weather);
+
+            File.WriteAllText("weather", convertToJson);
+        }
+
+        private static void DeSerializeFileNewtonsoftJson(string fileName)
+        {
+            var file = File.ReadAllText(fileName);
+            var weather = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherForeCast>(file);
+
+            Console.WriteLine(weather);
+        }
+
+        public static void DeSerializeFileMicrosoftTextJson(string fileName)
+        {
+            var file = File.ReadAllText(fileName);
+            var weather = System.Text.Json.JsonSerializer.Deserialize<WeatherForeCast>(file);
+
+            Console.WriteLine(weather);
+        }
+
+        //Serializer File
+        public static void SerializeFileMicrosoftTextJson(WeatherForeCast weather)
+        {
+            var convertToJson = System.Text.Json.JsonSerializer.Serialize(weather);
+
+            File.WriteAllText("weather", convertToJson);
+        }
+    }
+
+
+}
---
+++ EF Core JSON Processing/Lesson/Demo/Demo/WeatherForeCast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo
+{
+    public class WeatherForeCast
+    {
+        public DateTime DateTime { get; set; } = DateTime.UtcNow.Date;
+
+        public int TemperatureC { get; set; } = 30;
+
+        public string Summary { get; set; } = "Hot Summer Day";
+
+        public override string ToString()
+        {
+            return $"Date:{this.DateTime.ToString()} " +
+                $"Temp:{this.TemperatureC} Summary:{this.Summary}"; 
+        }
+
+    }
+}
