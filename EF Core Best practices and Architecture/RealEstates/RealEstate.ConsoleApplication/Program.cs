637c429862402a33b1408288c7df5e686169a7bc
mindball <mincho.balaliew@gmail.com>
2020-09-02 08:03:44 +0300

    Create ASP Core, Add services, Add DBContext with connection string

--- EF Core Best practices and Architecture/RealEstates/RealEstate.ConsoleApplication/Program.cs
+++ EF Core Best practices and Architecture/RealEstates/RealEstate.ConsoleApplication/Program.cs
@@ -16,20 +16,20 @@ namespace RealEstate.ConsoleApplication
             //db.Database.EnsureDeleted();
             db.Database.Migrate();
 
-            //IPropertiesService propertyService = new PropertiesService(db);
-            //var filteredProperties = propertyService.SearchByPrice(0, 20000);
-            //
-            //var squareMetterSymbol = "m\u00b2";
-            //foreach (var property in filteredProperties)
-            //{
-            //    Console.WriteLine($"{property.District}, " +
-            //        $"{property.Floor}, " +
-            //        $"{property.Size} {squareMetterSymbol}, " +
-            //        $"{property.Year}, " +
-            //        $"{property.Price} €, " +
-            //        $"{property.PropertyType}, " + 
-            //        $"{property.BuildingType}");
-            //}
+            IPropertiesService propertyService = new PropertiesService(db);
+            var filteredProperties = propertyService.SearchByPrice(0, 20000);
+
+            var squareMetterSymbol = "m\u00b2";
+            foreach (var property in filteredProperties)
+            {
+                Console.WriteLine($"{property.District}, " +
+                    $"{property.Floor}, " +
+                    $"{property.Size} {squareMetterSymbol}, " +
+                    $"{property.Year}, " +
+                    $"{property.Price} €, " +
+                    $"{property.PropertyType}, " +
+                    $"{property.BuildingType}");
+            }
 
             IDistrictsService districtService = new DistrictsService(db);
             var districts = districtService.GetTopDistrictByAveragePrice(100);
