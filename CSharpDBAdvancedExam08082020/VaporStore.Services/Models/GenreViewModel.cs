6fccc987a6657cdecbf8c990553c859e1c3b3acf
mindball <mincho.balaliew@gmail.com>
2020-10-05 10:38:27 +0300

    DTOs

---
+++ CSharpDBAdvancedExam08082020/VaporStore.Services/Models/GenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaporStore.Services.Models
+{
+    public class GenreViewModel
+    {
+        public int Id { get; set; }
+
+        public string Genre { get; set; }
+
+        public int TotalPlayers { get; set; }
+
+        public List<GamesViewModel> Games { get; set; }
+    }
+}
