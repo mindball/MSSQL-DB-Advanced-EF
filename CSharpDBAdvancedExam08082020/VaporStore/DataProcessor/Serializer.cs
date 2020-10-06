2c009f03e0b96f16a129a3bd449dade022ade189
mindball <mincho.balaliew@gmail.com>
2020-10-06 09:23:15 +0300

    complex LinqQuery

--- CSharpDBAdvancedExam08082020/VaporStore/DataProcessor/Serializer.cs
+++ CSharpDBAdvancedExam08082020/VaporStore/DataProcessor/Serializer.cs
@@ -23,6 +23,7 @@
 
 			IVaportService vaportService = new VaportService(context);
 
+			//using profile
 			var genres = vaportService.SearchGenres(Mapper)
 				.Where(g => genreNames.Contains(g.Genre))
 				.Select(g => new
@@ -36,19 +37,61 @@
 				})				
 				.ToList();
 
-			var jsonSettings = new Newtonsoft.Json.JsonSerializerSettings()
+			//using complex linq query author solution:
+			var complexLinqQuery = LinqQuery(context, genreNames);
+
+			var jsonSettings = new JsonSerializerSettings()
 			{
-				NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore
+				NullValueHandling = NullValueHandling.Ignore
 			};
 
-			string json = JsonConvert.SerializeObject(genres, Formatting.Indented);
+			//string jsonProfile = JsonConvert.SerializeObject(genres, Formatting.Indented);
 
-			return json;
+			string jsonLinqQuery = JsonConvert.SerializeObject(complexLinqQuery, Formatting.Indented);
+
+			//return jsonProfile;
+
+			return jsonLinqQuery;
 		}
 
-		public static string ExportUserPurchasesByType(VaporStoreContext context, string storeType)
+        public static string ExportUserPurchasesByType(VaporStoreContext context, string storeType)
 		{
 			throw new NotImplementedException();
+		}
+
+        private static object LinqQuery(VaporStoreContext context, 
+			string[] genreNames)
+        {
+			var linqQuery = context
+				.Genres
+				.ToArray()
+				.Where(g => genreNames.Contains(g.Name))
+				.Select(g => new
+				{
+					Id = g.Id,
+					Genre = g.Name,
+					Games = g.Games
+						.Where(ga => ga.Purchases.Any())
+						.Select(ga => new
+						{
+							Id = ga.Id,
+							Title = ga.Name,
+							Developer = ga.Developer.Name,
+							Tags = String.Join(", ", ga.GamesTags
+								.Select(gt => gt.Tag.Name)
+								.ToArray()),
+							Players = ga.Purchases.Count
+						})
+						.OrderByDescending(ga => ga.Players)
+						.ThenBy(ga => ga.Id)
+						.ToArray(),
+					TotalPlayers = g.Games.Sum(ga => ga.Purchases.Count)
+				})
+				.OrderByDescending(g => g.TotalPlayers)
+				.ThenBy(g => g.Id)
+				.ToArray();
+
+			return linqQuery;
 		}
 	}
 }
